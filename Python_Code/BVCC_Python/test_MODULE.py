2721ce78d43d67de8cc856c1c541e2204625c41e
chefzaq <izaac.ballard@bison.howard.edu>
2022-05-27 09:24:06 -0400

    commit on master

--- Python_Code/BVCC_Python/test_MODULE.py
+++ Python_Code/BVCC_Python/test_MODULE.py
@@ -1,17 +1,9 @@
-import numpy as np
 import unittest
-import pytest
-#import ipytest
-import doctest
-
 from MODULE import number_factors
 from MODULE import anagram
 from MODULE import factorial
 
 class TestMODULE(unittest.TestCase):
-    
-
-    
     def test_number_factors(self):
         assert number_factors(6) == [1,2,3,6]
         self.assertEqual(number_factors(0),[])
@@ -19,7 +11,6 @@ class TestMODULE(unittest.TestCase):
         self.assertEqual(number_factors(49),[1,7,49])
         self.assertNotEqual(number_factors(49),[1,49])
 
-    
     def test_anagram(self):
         #assert anagram('aNaGram', 'nag a ram') == True
         self.assertFalse(anagram('aNaGram', 'nag a ram') != True)
@@ -33,11 +24,5 @@ class TestMODULE(unittest.TestCase):
         self.assertEqual(factorial(2),2)
         self.assertFalse(factorial(3)>7)
 
-    
-
-#test_number_factors()
 if __name__ == "__main__":
-
     unittest.main()
-    #test_number_factors()
-    print("Everything passed")
